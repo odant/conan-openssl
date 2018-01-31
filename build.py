e7c6b93db5dba13bc56121d0c6c3371d6302f7e1
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-01-31 16:21:10 +0300

    Fix option ddl_sign

--- build.py
+++ build.py
@@ -37,7 +37,7 @@ def add_dll_sign(builds):
     result = []
     for settings, options, env_vars, build_requires, reference in builds:
         options = deepcopy(options)
-        options["zlib:dll_sign"] = dll_sign
+        options["openssl:dll_sign"] = dll_sign
         result.append([settings, options, env_vars, build_requires, reference])
     return result
     
