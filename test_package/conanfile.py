717720b4678998ab253230bec4934afb6be68bc9
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-04-07 01:38:30 +0300

    Test package verbose

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -7,6 +7,7 @@ class PackageTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
+        cmake.verbose = True
         cmake.configure()
         cmake.build()
 
@@ -17,6 +18,6 @@ class PackageTestConan(ConanFile):
 
     def test(self):
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
-            self.run("ctest --output-on-failure --build-config %s" % self.settings.build_type)
+            self.run("ctest --verbose --build-config %s" % self.settings.build_type)
         else:
-            self.run("ctest --output-on-failure")
+            self.run("ctest --verbose")
