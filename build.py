ffb49df150f9d8a2b24300f70afc3dd90867ef12
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2019-07-03 02:47:31 +0300

    Backport CI configs from branch 1.1.1

--- build.py
+++ build.py
@@ -1,3 +1,7 @@
+# OpenSSL Conan package
+# Dmitriy Vetutnev, ODANT, 2018-2019
+
+
 import platform, os
 from copy import deepcopy
 from conan.packager import ConanMultiPackager
@@ -9,15 +13,28 @@ username = "odant" if "CONAN_USERNAME" not in os.environ else None
 visual_versions = ["14", "15"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
 visual_runtimes = ["MD", "MDd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
 dll_sign = False if "CONAN_DISABLE_DLL_SIGN" in os.environ else True
+with_unit_tests = True if "WITH_UNIT_TESTS" in os.environ else False
+
 
 def add_dll_sign(builds):
     result = []
     for settings, options, env_vars, build_requires, reference in builds:
         options = deepcopy(options)
-        options["openssl:dll_sign"] = dll_sign
+        # Only for shared (shared is default)
+        if options.get("openssl:shared", True):
+            options["openssl:dll_sign"] = dll_sign
         result.append([settings, options, env_vars, build_requires, reference])
     return result
-    
+
+def add_with_unit_tests(builds):
+    result = []
+    for settings, options, env_vars, build_requires, reference in builds:
+        options = deepcopy(options)
+        options["openssl:with_unit_tests"] = with_unit_tests
+        result.append([settings, options, env_vars, build_requires, reference])
+    return result
+
+
 if __name__ == "__main__":
     builder = ConanMultiPackager(
         username=username,
@@ -25,11 +42,12 @@ if __name__ == "__main__":
         visual_runtimes=visual_runtimes,
         exclude_vcvars_precommand=True
     )
-    builder.add_common_builds(pure_c=True, shared_option_name="openssl:shared")
+    builder.add_common_builds(pure_c=True, shared_option_name=False)
     # Adjusting build configurations
     builds = builder.items
     if platform.system() == "Windows":
         builds = add_dll_sign(builds)
+    builds = add_with_unit_tests(builds)
     # Replace build configurations
     builder.items = []
     for settings, options, env_vars, build_requires, _ in builds:
