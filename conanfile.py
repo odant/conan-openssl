0fbcd0aa9ff18b0e7b91dee3826bace59a926c3a
d0nc1h0t <udincev@yandex.ru>
2021-08-25 10:56:34 +0300

    Version 1.1.1L+0

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ import os, glob
 
 class OpensslConan(ConanFile):
     name = "openssl"
-    version = "1.1.1i+1"
+    version = "1.1.1L+0"
     license = "The current OpenSSL licence is an 'Apache style' license: https://www.openssl.org/source/license.html"
     description = "OpenSSL is an open source project that provides a robust, commercial-grade, and full-featured " \
                   "toolkit for the Transport Layer Security (TLS) and Secure Sockets Layer (SSL) protocols"
